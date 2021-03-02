91ae1b824ee861e376b85aab7ceab675de3ac128
Shua <g.deglisezinski@gmail.com>
2021-03-02 23:36:59 +0100

    Login page update with navigation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Switch, Route } from 'react-router-dom';
 import Navigation from 'components/layout/Navigation';
 import Home from 'pages/Home';
 import Signup from 'pages/Signup';
+import Login from 'pages/Login';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/signup" component={Signup} />
+            <Route path="/signin" component={Login} />
         </Switch>
     </div>
   );
